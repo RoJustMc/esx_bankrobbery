8e49a780dc0ed99a7a1e24c0e90ba5a9fee5e4f4
Justus <99740029+RoJustMc@users.noreply.github.com>
2022-03-03 17:24:02 +0100

    Add files via upload

---
+++ t1ger_bankrobbery.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('drill', 'Drill', 15, 0, 1),
+('hackerDevice', 'Hacker Device', 10, 0, 1),
+('lockpick', 'Lockpick', 10, 0, 1),
+('hammerwirecutter', 'Hammer And Wire Cutter', 10, 0, 1),
+('accesscard', 'Access Card', 10, 0, 1),
+('goldbar', 'Gold Bar', 100, 0, 1),
+('goldwatch', 'Gold Watch', 200, 0, 1),
+('goldnecklace', 'Gold Necklace', 150, 0, 1),
+('diamond', 'Diamond', 10, 0, 1),
